bdd1f07dbed55a7debc3956dcef12c65c2dbe0b4
Trehinos <trehinos@gmail.com>
2025-06-13 01:41:07 +0200

    Add argument parsing with Clap

--- src/bin/hermes-client.rs
+++ src/bin/hermes-client.rs
@@ -1,9 +1,34 @@
+use clap::{Arg, ArgAction, Command};
 use hermes::client::Client;
+
+/// Version of the `hermes` crate used to build this binary.
+const VERSION: &str = env!("CARGO_PKG_VERSION");
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let url = std::env::args().nth(1).expect("URL required");
-    let response = Client::get(&url).await?;
+    let matches = Command::new("hermes-client")
+        .version(VERSION)
+        .disable_help_flag(false)
+        .disable_version_flag(true)
+        .arg(
+            Arg::new("version")
+                .short('v')
+                .long("version")
+                .action(ArgAction::Version)
+                .help("Print version information"),
+        )
+        .arg(
+            Arg::new("uri")
+                .value_name("URI")
+                .help("Destination URI")
+                .required(true),
+        )
+        .get_matches();
+
+    let url = matches
+        .get_one::<String>("uri")
+        .expect("URI argument missing");
+    let response = Client::get(url).await?;
     println!("{}", response);
     Ok(())
 }
--- src/bin/hermes-server.rs
+++ src/bin/hermes-server.rs
@@ -1,9 +1,48 @@
+use clap::{value_parser, Arg, ArgAction, Command};
 use hermes::server::Server;
+
+/// Version of the `hermes` crate used to build this binary.
+const VERSION: &str = env!("CARGO_PKG_VERSION");
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let port = std::env::args().nth(1).unwrap_or_else(|| "80".to_string());
-    let addr = format!("0.0.0.0:{}", port);
+    let matches = Command::new("hermes-server")
+        .version(VERSION)
+        .disable_help_flag(false)
+        .disable_version_flag(true)
+        .arg(
+            Arg::new("version")
+                .short('v')
+                .long("version")
+                .action(ArgAction::Version)
+                .help("Print version information"),
+        )
+        .arg(
+            Arg::new("address")
+                .short('a')
+                .long("address")
+                .value_name("ADDRESS")
+                .default_value("0.0.0.0")
+                .help("Address to bind"),
+        )
+        .arg(
+            Arg::new("port")
+                .short('p')
+                .long("port")
+                .value_name("PORT")
+                .default_value("80")
+                .value_parser(value_parser!(u16))
+                .help("Port to listen on"),
+        )
+        .get_matches();
+
+    let address = matches
+        .get_one::<String>("address")
+        .expect("address has default");
+    let port = matches
+        .get_one::<u16>("port")
+        .expect("port has default");
+    let addr = format!("{}:{}", address, port);
     let server = Server::new(&addr);
     server.run().await
 }
